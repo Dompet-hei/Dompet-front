b449b14b67f93b5e529b9c7281993c7590121647
tendryAxel <hei.tendry@gmail.com>
2024-03-18 22:47:01 +0300

    feat: theme color

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,3 +1,21 @@
 export const theme = {
-  base: "grey",
+  light: "#B8EBD0",
+  dark: "#292F36",
+  alert: "#FF6B6B",
+  base: "#B0C7BD",
+};
+
+export const opacityTheme = {
+  light: createApplyOpacityFunction(theme.light),
+  dark: createApplyOpacityFunction(theme.dark),
+  alert: createApplyOpacityFunction(theme.alert),
+  base: createApplyOpacityFunction(theme.base),
+};
+
+export const applyOpacity = (color, opacity) => {
+  return `${color}${opacity.toString(16)}`;
+};
+
+const createApplyOpacityFunction = (color) => {
+  return (o) => applyOpacity(color, o);
 };
