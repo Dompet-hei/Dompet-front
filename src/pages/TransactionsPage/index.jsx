6d22039d4d600eb0736092c366e6054da51a3486
tendryAxel <hei.tendry@gmail.com>
2024-04-01 22:28:36 +0300

    feat: place to put statement function

--- src/pages/TransactionsPage/index.jsx
+++ src/pages/TransactionsPage/index.jsx
@@ -8,6 +8,8 @@ import {
   Tr,
   Th,
   Button,
+  Input,
+  Text,
 } from "@chakra-ui/react";
 import Row from "./components/Row";
 import { useDisclosure } from "@chakra-ui/react";
@@ -18,9 +20,14 @@ import { useContext } from "react";
 import { FetchContext } from "../../FetchProvider";
 import { UserContext } from "../../UserProvider";
 import Graph from "./components/graph";
+import { useBoolean } from "usehooks-ts";
+import useInput from "../../hooks/useInput";
+import { opacityTheme, theme } from "../../utils/color";
 
 export default () => {
   const [transactions, setTransactions, ,] = useList();
+  const [startDate, onChangeStartDate, clearStartDate] = useInput(null);
+  const [endDate, onChangeEndDate, clearEndDate] = useInput(null);
 
   const { id } = useContext(UserContext);
   const { verb } = useContext(FetchContext);
@@ -32,8 +39,8 @@ export default () => {
   useEffect(() => {}, [transactions]);
 
   return (
-    <Flex>
-      <TableContainer w="50%">
+    <Flex direction="row" wrap="wrap">
+      <TableContainer w="40%">
         <Table w="100%" variant="striped" colorScheme="teal">
           <Thead>
             <Row
@@ -55,7 +62,34 @@ export default () => {
           </Tbody>
         </Table>
       </TableContainer>
-      <Graph content={transactions} />
+      {startDate && endDate ? <></> : <Graph content={transactions} />}
+      <Flex
+        w="100%"
+        h="7em"
+        bgColor={opacityTheme.base(0.7)}
+        justifyContent="space-evenly"
+        alignItems="center"
+        wrap="wrap"
+      >
+        <Text fontSize="1.5em">Create statement</Text>
+        <Text>Between</Text>
+        <Input w="25%" type="date" onChange={onChangeStartDate} />
+        <Text>and</Text>
+        <Input w="25%" type="date" onChange={onChangeEndDate} />
+        {!startDate || !endDate ? (
+          <></>
+        ) : (
+          <Button
+            colorScheme="red"
+            onClick={() => {
+              clearStartDate();
+              clearEndDate();
+            }}
+          >
+            Clear
+          </Button>
+        )}
+      </Flex>
     </Flex>
   );
 };
